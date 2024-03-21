e0e1d848ec81e84a0a08ca726bbdad08f096b7f9
aeprodigy <mikeapple056@gmail.com>
2024-03-21 15:01:10 +0200

    contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Contact = () => {
+  //Api key
+  const API_KEY =
+    "live_iFt3Tqo8B3oZO1LKao2mxS7QjTN55bJUmx9ykU0ruQhwoJXc9B1jmcFFcBeqsj5I";
+
+  //states
+  const [data, setData] = useState([]);
+
+  const fetchCats = () => {
+    axios(
+      `https://api.thecatapi.com/v1/images/search?limit=1&api_key=${API_KEY}`
+    )
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching cat images:", error);
+      });
+  };
+  //useState effect
+  useState(() => {
+    fetchCats();
+  }, []);
+
   return (
-    <div>Contact</div>
-  )
+    <div className='max-w-[1200px] m-auto w-full p-4 py-16 '>
+      <h2 className='text-center text-gray-700 text-2xl '>Send us a Message</h2>
+      <p className='text-center text-gray-700 py-2'>We're waiting on You</p>
+      <div>
+        {data.map((item) => (
+          <div key={item.id}>
+            <img src={item.url} alt="/" />
+          </div>
+        ))}
+        <form>
+          <div>
+            <input type="text" placeholder="first" />
+            <input type="text" placeholder="last" />
+            <input type="email" placeholder="email" />
+            <input type="tel" placeholder="phone" />
+            <input type="text" placeholder="address" />
+            <textarea cols='30' rows='10' placeholder='mssg'></textarea>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact
